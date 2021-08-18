cf22d43b27d1993803a3bbc0c04092918cc30bd9
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-08-17 22:30:51 -0500

    Week 2 Assignment 2.1

--- week2/assignment2.1/recursion.py
+++ week2/assignment2.1/recursion.py
@@ -1,4 +1,3 @@
-
 def loop1():
     # Sum the odd numbers between 1 and 20
     odd_sum = 0
@@ -7,18 +6,13 @@ def loop1():
             odd_sum += i
     return odd_sum
 
-def loop1rec(i, odd_sum):
-    print('function call')
+def loop1rec(i,odd_sum):
+    # Duplicate the loop1 function using recursion
     if i >= 20:
         return odd_sum
     elif (i % 2) == 1:
         odd_sum += i
     return loop1rec(i+1,odd_sum)
-
-print(loop1())
-i=0
-odd_sum=0
-print(loop1rec(i,odd_sum))
 
 def loop2():
     # Sum the even numbers between 1 and 20
@@ -30,10 +24,27 @@ def loop2():
         i += 1
     return even_sum
 
-#def loop1Rec(num,odd_sum):
-    # Duplicate the loop1 function using recursion
-
-    
-
-#def loop2Rec(num,even_sum):
+def loop2Rec(i,even_sum):
     # Duplicate the loop2 function using recursion
+    if i == 20:
+        return even_sum
+    elif (i % 2) == 0:
+        even_sum += i
+    i += 1
+    return loop2Rec(i+1,even_sum)
+
+# Call each of the four functions and print the results for each:
+
+print('Sum of odds between 1 and 20 using \'for\' loop =',loop1())
+
+i=0
+odd_sum=0
+
+print('Sum of odds between 1 and 20 using recursion =',loop1rec(i,odd_sum))
+
+print('Sum of evens between 1 and 20 using \'while\' loop =',loop2())
+
+i=0
+even_sum=0
+
+print('Sum of evens between 1 and 20 using recursion =',loop2Rec(i,even_sum))
