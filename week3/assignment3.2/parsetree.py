f3036f98635b04fc6bc0569ab5864f0123f536bc
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-08-31 12:35:00 -0500

    Assignment 3.2: Parse Tree

--- week3/assignment3.2/parsetree.py
+++ week3/assignment3.2/parsetree.py
@@ -16,34 +16,27 @@ def buildParseTree(exp):
     explist = exp.split()
     currentNode = root
     
-    print('root: ', root.val)
-    
     for expression in explist:
+        
+        print('expression: ', expression)
 
         #left parentheses, right parentheses, operators, and operands.
         if expression == '(':
             
-            #print('expression: ', expression)
-            
             # create a node and make it the left of the currentNode
-            node = Node(expression)
+            node = Node('')
             currentNode.left = node
-            #newNode.left = currentNode.left
-            
-            #print('newNode: ', newNode.val)
-            
+            currentNode.left.parent = currentNode
+ 
             # then descent to that left child (ie: currentNode equal to left child)
-            currentNode = node
-            
-            #print('currentNode: ', currentNode.val)
+            currentNode = currentNode.left
             
         elif expression == ')':
+
             # go up to the parent of the currentNode
             currentNode = currentNode.parent
 
         elif expression in '+-*/':
-            
-            #print('expression: ', expression)
             
             # set value of the currentNode to that operator
             currentNode.val = expression
@@ -53,53 +46,49 @@ def buildParseTree(exp):
             node = Node(expression)
             currentNode.right = node
    
-            #print('newNode: ', newNode.val)
-
             # then descend to that right child
             currentNode = node
-            
-            #print('currentNode: ', currentNode.val)
 
-        #elif expression not in '+-*/':
+        elif expression not in '+-*/':
             # expression is a number
             # set value of the currentNode to that number
             currentNode.val = expression
             
             # go back up to our parent
-            #currentNode = root
+            currentNode = root
             
         #else:
             #raise ValueError("Unknown Operator: " + expression)
 
 #Create a function to traverse and print the parse tree in order .  You will need to use recursion.
-def inOrder(node):
+#def inOrder(node):
     # - Recursively do a inorder traversal of the left subtree
     # - Visit root node first
     # - Recursively do a inorder traversal of the right subtree
-    if node:
-        inOrder(node.left)
-        print(node.val)
-        inOrder(node.right)
+    #if node:
+        #inOrder(node.left)
+        #print(node.val)
+        #inOrder(node.right)
         
 #Create a function to traverse and print the parse tree in pre-order. You will need to use recursion.
-def inPreOrder(node):
+#def inPreOrder(node):
     # - Recursively do a preorder traversal of the left subtree
     # - Visit root node first
     # - Recursively do a preorder traversal of the right subtree
-    if node:
-        inPreOrder(node.left)
-        print(node.val)
-        inPreOrder(node.right)
+    #if node:
+        #inPreOrder(node.left)
+        #print(node.val)
+        #inPreOrder(node.right)
     
 #Create a function to traverse and print the parse tree in post-order. You will need to use recursion.
-def inPostOrder(node):
+#def inPostOrder(node):
     # - Recursively do a postorder traversal of the left subtree
     # - Visit root node first
     # - Recursively do a postorder traversal of the right subtree
-    if node:
-        inPostOrder(node.left)
-        print(node.val)
-        inPostOrder(node.right)
+    #if node:
+        #inPostOrder(node.left)
+        #print(node.val)
+        #inPostOrder(node.right)
     
 #buildParseTree("( ( 11 * 2 ) + ( 10 - 2 ) )")
 buildParseTree("( 11 * 2 )")
