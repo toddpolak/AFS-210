02fda7dfc5e853537740476be05432171af21cf2
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-09-03 19:14:13 -0500

    Assignment 5.1: Searching

--- week5/searching/binarysearch.py
+++ week5/searching/binarysearch.py
@@ -1,7 +1,12 @@
 # binary search on a sorted list
 def binary_search(list, term):
     
-    return False
+        size_of_list = len(list) - 1 
+
+        index_of_first_element = 0 
+        index_of_last_element = size_of_list 
+    
+        return False
 
 list1 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 list2 = ["Alpha", "Beta", "Delta", "Epsilon", "Gamma", "Theta", "Zeta"]
