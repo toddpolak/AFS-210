bcebcd8b1ee4d7ea07f3e8a07836e03d7d7ff59a
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-09-03 20:56:11 -0500

    Assignment 5.1: Searching

--- week5/searching/binarysearch.py
+++ week5/searching/binarysearch.py
@@ -1,16 +1,21 @@
 # binary search on a sorted list
-def binary_search(list, term):
+def binary_search(list, fst_elm_idx, lst_elm_idx, term):
+        
+        if (lst_elm_idx < fst_elm_idx):
+                return False
+        else:
+                mid = fst_elm_idx + ((lst_elm_idx - fst_elm_idx) // 2)
+                
+                if (list[mid] > term):
+                        return binary_search(list, fst_elm_idx, mid - 1, term)
+                elif (list[mid] < term):
+                        return binary_search(list, mid + 1, lst_elm_idx, term)
+                else:
+                        return True
     
-        size_of_list = len(list) - 1 
-
-        index_of_first_element = 0 
-        index_of_last_element = size_of_list 
-    
-        return False
-
 list1 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 list2 = ["Alpha", "Beta", "Delta", "Epsilon", "Gamma", "Theta", "Zeta"]
 
-print(binary_search(list1, 31))
-print(binary_search(list1, 77))
-print(binary_search(list2, "Delta"))
+print(binary_search(list1, 0, len(list1)-1, 31))
+print(binary_search(list1, 0, len(list1)-1, 77))
+print(binary_search(list2, 0, len(list2)-1, "Delta"))
