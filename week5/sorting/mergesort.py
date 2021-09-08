1ef0fbea695136b503b9408e8013367b2030077e
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-09-07 19:18:10 -0500

    Assignment 5.2: Merge Sort

--- week5/sorting/mergesort.py
+++ week5/sorting/mergesort.py
@@ -1,9 +1,9 @@
 def mergeSort(nlist):
-
-    # insert your code to complete the mergeSort function
-
+    
     print("Splitting ", nlist)
 
+    # insert your code to complete the mergeSort function
+    
     list_length = len(nlist)
     
     if list_length == 1:
@@ -12,9 +12,9 @@ def mergeSort(nlist):
     mid_point = list_length // 2
 
     left_side = mergeSort(nlist[:mid_point])
-
+    
     right_side = mergeSort(nlist[mid_point:])
-
+    
     print("Merging ", nlist)
 
     return merge(nlist, left_side, right_side)
@@ -41,7 +41,7 @@ def merge(nlist, lefthalf, righthalf):
         k=k+1
     return nlist
 
-myList = [ 55, 31, 26 ,20 ,63 ,7 ,51 ,74 ,81 ,40 ]
+myList = [ 55, 31, 26, 20, 63, 7, 51, 74, 81, 40 ]
 
 mergeSort(myList)
 
