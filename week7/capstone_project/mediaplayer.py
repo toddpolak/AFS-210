c93ca11416ec52328ead5e7702a23658184342b1
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-09-11 07:13:38 -0500

    Capstone Project

---
+++ week7/capstone_project/mediaplayer.py
@@ -0,0 +1,82 @@
+
+class Song:
+    def __init__(self,title,artist):
+        self.title = title
+        self.artist = artist
+
+    def getTitle(self):
+        return self.title
+
+    def getArtist(self):
+        return self.artist
+        
+    def __str__(self):
+        return self.title + " by " + self.artist 
+
+    def __eq__(self, other):
+        return ((self.title, self.artist) == (other.title, other.artist))
+        
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __lt__(self, other):
+        return ((self.title, self.artist) < (other.title, other.artist))
+        
+    def __gt__(self, other):
+        return ((self.title, self.artist) < (other.title, other.artist))
+        
+
+
+def menu():
+    print(20 * "-" , "MENU" , 20 * "-")
+    print("1. Add Song to Playlist")
+    print("2. Remove song from Playlist")
+    print("3. Play")
+    print("4. Skip")
+    print("5. Go Back")
+    print("6. Shuffle")
+    print("7. Show Currently Playing Song")
+    print("8. Show Current Playlist Order")
+    print("0. Exit")
+    print(47 * "-")
+
+
+while True:
+    menu()
+    choice = int(input())
+
+    if choice == 1:
+        # Add code to prompt user for Song Title and Artist Name
+        # Add song to playlist
+        print("New Song Added to Playlist")
+    elif choice == 2:
+        # Prompt user for Song Title 
+        # remove song from playlist
+        print("Song Removed to Playlist")
+    elif choice == 3:
+        # Play the playlist from the beginning
+        # Display song name that is currently playing
+        print("Playing....")        
+    elif choice == 4:
+        # Skip to the next song on the playlist
+        # Display song name that is now playing
+        print("Skipping....")                     
+    elif choice == 5:
+        # Go back to the previous song on the playlist
+        # Display song name that is now playing
+        print("Replaying....")  
+    elif choice == 6:
+        # Randomly shuffle the playlist and play the first song
+        # Display song name that is now playing
+        print("Shuffling....")          
+    elif choice == 7:
+        # Display the song name and artist of the currently playing song
+        print("Currently playing: ", end=" ")    
+    elif choice == 8:
+        # Show the current song list order
+        print("\nSong list:\n")
+    elif choice == 0:
+        print("Goodbye.")
+        break
+
+            
