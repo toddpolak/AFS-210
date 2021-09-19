1bb0a239a4bf84a649fcccf24d9f6f5ae7a6482b
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-09-18 23:25:53 -0500

    Capstone Project

--- week7/capstone_project/mediaplayer.py
+++ week7/capstone_project/mediaplayer.py
@@ -33,92 +33,103 @@ class Song():
 # Queue Class
 #region
 class Queue:
-    def __init__(self, current_song_index=0):
+    def __init__(self, current_song_index=0, currently_playing=False):
         self.items = []
         self.set_current_song_index(current_song_index)
-        
-    def get_current_song_index(self):
-        return (self._current_song_index)
-    
+        self.set_currently_playing(currently_playing)
+
     def set_current_song_index(self, index):
-        #self._current_song_index = index
         self.current_song_index = index
 
+    def set_currently_playing(self, currently_playing):
+        self.curently_playing = currently_playing
+
     def add_song(self, song):
-        self.items.insert(0, song)
-        
+        if self.size() > 0:
+            self.items.insert(self.size() + 1, song)
+        else:
+            self.items.insert(0, song)
+
     def size(self):
         return len(self.items)
-        
+
     def play(self, song_index):
-        
-        print("\nPlaying....")
-        
+
         self.current_song_index = song_index
         
-        print(self.items[self.current_song_index])
+        self.set_currently_playing(True)
         
+        print("\nPlaying....")
+
+        print(self.items[self.current_song_index])
+
     def show_play_list(self):
         num = 1
-        
+
         print("\nSong list:\n")
-        
+
         for item in self.items:
             print(str(num) + '. ' + str(item))
             num += 1
-            
+
     def show_current_song(self):
-        
         print("\nCurrently playing:")
-        
+
         print(self.items[self.current_song_index])
-        
+
     def next(self):
         
-        # current song index
-        #print('song index: ', self.current_song_index)
+        print('Currently Playing: ', self.curently_playing)
         
         playlist = len(self.items) - 1
-        #cur_song = current_song_index
-   
-        # length of playlist -1
-        #print('length of playlist: ', playlist)
-        
+
         if self.current_song_index == playlist:
             next_song = 0
         else:
             next_song = self.current_song_index + 1
-            
+
         print("\nSkipping....")
-            
+
         self.play(next_song)
-        
+
     def prev(self):
-        
         playlist = len(self.items) - 1
-        
-        #print('playlist: ', playlist)
-        #print('current song index: ', self.current_song_index)
 
         if self.current_song_index == playlist:
             prev_song = 0
         else:
             prev_song = self.current_song_index - 1
-            
-        print("Replaying....")
-        
+
+        print("\nReplaying....")
+
         self.play(prev_song)
-        
 
     def removeSong(self, title):
         song_index = 0
-        
+
         for item in self.items:
             if item.title == title:
                 break   
             song_index += 1
-        
+
         self.items.pop(song_index)
+        
+    def shuffle(self):
+        list_length = self.size()
+        
+        for i in range(list_length):
+            
+            # pop off the first element and append it to the end of the list
+            first_number = self.items.pop(0)
+            self.items.append(first_number)
+            
+            # generate a random number based on the size of the list
+            random_num = random.randint(0, list_length - 1)
+
+            # pop off that random element and append to the end of the list
+            list_element = self.items.pop(random_num)
+            self.items.append(list_element)
+
 #endregion
 
 # Menu 
@@ -147,12 +158,9 @@ media_player.add_song(Song('What Lovers Do', 'Maroon 5'))
 media_player.add_song(Song('Heartbreaker', 'Mariah Carey'))
 media_player.add_song(Song('24K Magic', 'Bruno Mars'))
 media_player.add_song(Song('Wifey', 'Next'))
-
-#media_player.add_song(Song('Title 5', 'Artist 5'))
-#media_player.add_song(Song('Title 4', 'Artist 4'))
-#media_player.add_song(Song('Title 3', 'Artist 3'))
-#media_player.add_song(Song('Title 2', 'Artist 2'))
-#media_player.add_song(Song('Title 1', 'Artist 1'))
+media_player.add_song(Song('Rockit', 'Herbie Hancock'))
+media_player.add_song(Song('Dress You Up', 'Madonna'))
+media_player.add_song(Song('Midas Touch', 'Midnight Star'))
 
 # Media Player
 #region
@@ -172,62 +180,57 @@ while True:
         print("New Song: " + song.title + " Added to Playlist")
         
     elif choice == 2:
+    
         # Prompt user for Song Title 
         title = input('Enter the song title to be removed: ')
         
         # remove song from playlist
         media_player.removeSong(title)
         
-        print("Song: " + song.title + " Removed from Playlist")
+        print("Song: " + title + " Removed from Playlist")
 
     elif choice == 3:
         # Play the playlist from the beginning
         # Display song name that is currently playing
-        
-        #print("Playing....")
-        
-        #media_player.play(current_song_index)
+
         media_player.play(0)
         
     elif choice == 4:
         # Skip to the next song on the playlist
         # Display song name that is now playing
-        #print("Skipping....")
-        
+
         media_player.next()
         
     elif choice == 5:
         # Go back to the previous song on the playlist
         # Display song name that is now playing
-        #print("Replaying....")
-        
+
         media_player.prev()
 
     elif choice == 6:
         # Randomly shuffle the playlist and play the first song
         # Display song name that is now playing
-        
-        random.shuffle(media_player.items)
-        
+
+        media_player.shuffle()
+
         print("Shuffling....")
-        
+
         media_player.show_play_list()
-        
+
+        media_player.play(0)
+
     elif choice == 7:
+
         # Display the song name and artist of the currently playing song
-        #print("Currently playing: ", end=" ")
         media_player.show_current_song()
 
     elif choice == 8:
+
         # Show the current song list order
-        #print("\nSong list:\n")
-        
         media_player.show_play_list()
-        
-        #for item in media_player.items:
-            #print(item)
-        
+
     elif choice == 0:
+
         print("Goodbye.")
         break
 #endregion
