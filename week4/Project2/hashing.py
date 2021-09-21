1b07480145e2bb7596806fe83199e2bba0811191
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-09-21 09:49:50 -0500

    Project 2: Hashing

--- week4/Project2/hashing.py
+++ week4/Project2/hashing.py
@@ -6,15 +6,24 @@ class HashTable:
 
     def hashfunction(self,key):
         # Insert your hashing function code
+        keystr = str(key)
+        hashVal = 0
+        for ch in str(keystr):
+            hashVal += ord(ch)
+            
+        return (hashVal % self.size)
 
     def rehash(self,key):
         # Insert your secondary hashing function code
+        return
 
     def put(self,key,data):
         # Insert your code here to store key and data 
+        return
 
     def get(self,key):
         # Insert your code here to get data by key
+        return
 
     def __getitem__ (self,key):
         return self.get(key)
